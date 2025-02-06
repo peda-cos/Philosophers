83eb98cd93f2c0aff899937918434e742c64a82c
Pedro da Costa e Silva Monteiro <83892026+peda-cos@users.noreply.github.com>
2025-02-06 01:45:44 -0300

    feat: implement philosopher routine in routine.c

---
+++ philo/routine.c
@@ -0,0 +1,73 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   routine.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: peda-cos <peda-cos@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/06 01:15:33 by peda-cos          #+#    #+#             */
+/*   Updated: 2025/02/06 01:45:24 by peda-cos         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+static void	take_forks(t_philo *philo)
+{
+	if (philo->id % 2 == 0)
+	{
+		pthread_mutex_lock(philo->left_fork);
+		print_message(philo, "has taken a fork");
+		pthread_mutex_lock(philo->right_fork);
+		print_message(philo, "has taken a fork");
+	}
+	else
+	{
+		pthread_mutex_lock(philo->right_fork);
+		print_message(philo, "has taken a fork");
+		pthread_mutex_lock(philo->left_fork);
+		print_message(philo, "has taken a fork");
+	}
+}
+
+static void	eat_sleep_think(t_philo *philo)
+{
+	philo->last_meal = get_time();
+	print_message(philo, "is eating");
+	usleep(philo->data->time_to_eat * 1000);
+	philo->meals_eaten++;
+	pthread_mutex_unlock(philo->left_fork);
+	pthread_mutex_unlock(philo->right_fork);
+	print_message(philo, "is sleeping");
+	usleep(philo->data->time_to_sleep * 1000);
+	print_message(philo, "is thinking");
+}
+
+void	*philo_routine(void *arg)
+{
+	t_philo	*philo;
+
+	philo = (t_philo *)arg;
+	if (philo->data->number_of_philosophers == 1)
+	{
+		pthread_mutex_lock(philo->left_fork);
+		print_message(philo, "has taken a fork");
+		usleep(philo->data->time_to_die * 1000);
+		pthread_mutex_unlock(philo->left_fork);
+		return (NULL);
+	}
+	while (1)
+	{
+		pthread_mutex_lock(&philo->data->simulation_mutex);
+		if (philo->data->simulation_end)
+		{
+			pthread_mutex_unlock(&philo->data->simulation_mutex);
+			break ;
+		}
+		pthread_mutex_unlock(&philo->data->simulation_mutex);
+		take_forks(philo);
+		eat_sleep_think(philo);
+		usleep(100);
+	}
+	return (NULL);
+}
