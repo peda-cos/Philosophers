21c328ecac098f5b42b3a1f4180d2a36024f26a6
Pedro da Costa e Silva Monteiro <83892026+peda-cos@users.noreply.github.com>
2025-02-06 01:00:15 -0300

    feat: update philo.h with additional fields in t_philo struct

--- philo/philo.h
+++ philo/philo.h
@@ -6,7 +6,7 @@
 /*   By: peda-cos <peda-cos@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/05 00:58:54 by peda-cos          #+#    #+#             */
-/*   Updated: 2025/02/05 01:45:43 by peda-cos         ###   ########.fr       */
+/*   Updated: 2025/02/06 00:59:48 by peda-cos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,21 +22,30 @@
 
 typedef struct s_data
 {
-	int			number_of_philosophers;
-	int			time_to_die;
-	int			time_to_eat;
-	int			time_to_sleep;
-	long long	start_time;
-}				t_data;
+	int				number_of_philosophers;
+	int				time_to_die;
+	int				time_to_eat;
+	int				time_to_sleep;
+	int				must_eat;
+	long long		start_time;
+	pthread_mutex_t	*forks;
+	pthread_mutex_t	print_mutex;
+	pthread_mutex_t	simulation_mutex;
+	int				simulation_end;
+}					t_data;
 
 typedef struct s_philo
 {
-	int			id;
-	pthread_t	thread;
-	t_data		*data;
-}				t_philo;
+	int				id;
+	long long		last_meal;
+	int				meals_eaten;
+	pthread_t		thread;
+	t_data			*data;
+	pthread_mutex_t	*left_fork;
+	pthread_mutex_t	*right_fork;
+}					t_philo;
 
-int				mini_atoi(const char *str);
-long long		get_time(void);
+int					mini_atoi(const char *str);
+long long			get_time(void);
 
 #endif
