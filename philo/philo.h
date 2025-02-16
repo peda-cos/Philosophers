157faeee03d33decd93f2679bd7a19712535cabc
Pedro da Costa e Silva Monteiro <83892026+peda-cos@users.noreply.github.com>
2025-02-16 19:27:08 -0300

    refactor(philo.h): reorganize function declarations for consistency and readability

--- philo/philo.h
+++ philo/philo.h
@@ -6,7 +6,7 @@
 /*   By: peda-cos <peda-cos@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/05 00:58:54 by peda-cos          #+#    #+#             */
-/*   Updated: 2025/02/16 17:21:42 by peda-cos         ###   ########.fr       */
+/*   Updated: 2025/02/16 19:26:00 by peda-cos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,23 +45,24 @@ typedef struct s_philo
 	pthread_mutex_t	meal_mutex;
 }					t_philo;
 
-void    philosopher_take_forks(t_philo *philo);
-void	philosopher_eat(t_philo *philo);
-void	philosopher_release_forks(t_philo *philo);
-void	philosopher_sleep(t_philo *philo);
-void	philosopher_think(t_philo *philo);
+void				philosopher_take_forks(t_philo *philo);
+void				philosopher_eat(t_philo *philo);
+void				philosopher_release_forks(t_philo *philo);
+void				philosopher_sleep(t_philo *philo);
+void				philosopher_think(t_philo *philo);
 
-int	initialize_simulation_data(t_data *data, int argc, char **argv);
-t_philo	*initialize_philosophers(t_data *data);
-void	cleanup_simulation(t_data *data, t_philo *philos);
+int					initialize_simulation_data(t_data *data, int argc,
+						char **argv);
+t_philo				*initialize_philosophers(t_data *data);
+void				cleanup_simulation(t_data *data, t_philo *philos);
 
-void	monitor_philosophers(t_philo *philos, t_data *data);
+void				monitor_philosophers(t_philo *philos, t_data *data);
 
-void	*philosopher_routine(void *arg);
+void				*philosopher_routine(void *arg);
 
-int	mini_atoi(const char *str);
-long long	get_current_time(void);
-void	print_message(t_philo *philo, char *msg);
-void	print_death_message(t_philo *philo, char *msg);
+int					mini_atoi(const char *str);
+long long			get_current_time(void);
+void				print_message(t_philo *philo, char *msg);
+void				print_death_message(t_philo *philo, char *msg);
 
 #endif
